69600853ab0a9d464b0414ae83d3710c2e78cc18
Tamim Sharif <202314083@student.mist.ac.bd>
2025-03-20 02:51:54 +0600

    problem-4 solved

---
+++ string.js
@@ -0,0 +1,15 @@
+const name='Tamim Sharif';
+const address='Mirpur-12';
+const FrndUni='BUET';
+const fan='Disha Patani';
+const movieOfTheYear='Movie Dekhi Na';
+const hobby='Ethical hacking';
+const color='Black';
+console.log(name);
+console.log(address);
+console.log(FrndUni);
+console.log(fan);
+console.log(movieOfTheYear);
+console.log(hobby);
+console.log(color);
+
