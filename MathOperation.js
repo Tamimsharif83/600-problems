90266a549ecb6b0a2f1bb9c0b6e33043c4d66ba8
Tamim Sharif <202314083@student.mist.ac.bd>
2025-03-20 13:05:15 +0600

    problem-6 solved

---
+++ MathOperation.js
@@ -0,0 +1,37 @@
+const Income=100;
+const waste=40;
+console.log(Income-waste);
+const Pencil=10;
+const use=2;
+console.log(Pencil/use);
+const TotalTk=60;
+const Peruse=15;
+console.log("Per day use",TotalTk/Peruse);
+console.log("Thakbe ",TotalTk%Peruse);
+const s1="Hello";
+const s2="World";
+console.log(s1+s2);
+const totalRice =153;
+const totalDal=261;
+console.log(totalDal+totalRice);
+const TotalTaka=500;
+const Use=75;
+console.log("Use korte parbo ", Math.floor(TotalTaka / Use), "bar");
+console.log("Theke Jabe ",TotalTaka%Use,"Taka");
+console.log(8/3);
+console.log(8%3);
+console.log(50%9);
+const S3='Bangla';
+const S4='desh';
+console.log(S3+S4);
+const Rice=480;
+const Ricewaste=4;
+const survive=Rice/Ricewaste;
+console.log(survive,"din colbe");
+console.log(survive/30," mash colbe");
+
+
+
+
+
+
