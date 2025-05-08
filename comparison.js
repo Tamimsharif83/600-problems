5f464af895d70602942aea7ebf842ece3956c781
Tamim Sharif <202314083@student.mist.ac.bd>
2025-05-08 23:39:35 +0600

    comparison practice code added

---
+++ comparison.js
@@ -0,0 +1,22 @@
+//1
+const mySandwice=7;
+const brotherSandwice=5;
+console.log(mySandwice>brotherSandwice);
+//2
+const myClassStudent=45;
+const otherClassStudent=50;
+console.log(myClassStudent<otherClassStudent);
+//3
+const myNumber=75;
+const myFriendNumber=75;
+console.log(myNumber==myFriendNumber);
+//4
+const mySavings=95;
+console.log(mySavings<=100);
+//5
+console.log(10!=7);
+//6
+console.log(20<=15);
+//7
+console.log(97>=45);
+
