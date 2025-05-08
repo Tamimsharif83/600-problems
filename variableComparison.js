1b4b60a1467fdc10bca30a6178564f7965be97e8
Tamim Sharif <202314083@student.mist.ac.bd>
2025-05-09 00:04:27 +0600

    comparison of variable  practice code added

---
+++ variableComparison.js
@@ -0,0 +1,33 @@
+//1
+const myBrotherAge=10;
+const myAge=15;
+console.log(myBrotherAge<=myAge);
+//2
+const passMarks=33;
+const myMarks=45;
+console.log(myMarks>=passMarks);
+//3
+const jamburaPrice=40;
+const kodbelPrice=20;
+console.log(jamburaPrice>kodbelPrice);
+//4
+const myReadingBooks=10;
+const friendReadingBooks=10;
+console.log(myReadingBooks==friendReadingBooks);
+//5
+const myBiscuts=15;
+const friendBiscut=20;
+console.log(myBiscuts<friendBiscut);
+//6
+const goalOfReal=2;
+const goalOfBarcha=3;
+console.log(goalOfReal<goalOfBarcha);
+//7
+const myName="Rahim";
+const friendName="Karim";
+console.log(myName==friendName);
+//8
+const myReading=true;
+const friendReading=false;
+console.log(myReading==friendReading);
+
