3bf0a2b85f8520c35b1b75ae790cd69cf8797aa8
Tamim Sharif <202314083@student.mist.ac.bd>
2025-05-11 12:17:29 +0600

    conditional  problem  practice code added

---
+++ conditional.js
@@ -0,0 +1,54 @@
+//1
+const reachHome=7;
+if(reachHome<6)
+{
+    console.log("Nasta Khawabe");
+
+}
+else
+{
+    console.log("jarur bari khabo");
+}
+//2
+const logIn="success";
+if(logIn=="success")
+    console.log("Welcome!!");
+else
+{
+    console.log("Get Lost!!");
+
+}
+//3
+const dailyRun=5;
+if(dailyRun>=5)
+    console.log("take Your Chocolate!!");
+else{
+    console.log("Tor vuri mota!!");
+}
+//4
+const passNum=85;
+if(passNum>=80)
+{
+    console.log("Baba: Ei ne tor bike");
+
+}
+else{
+    console.log("Baba: Bike pabina!!");
+}
+//5
+const movieTime="9.30 p.m";
+if(movieTime=="9 p.m")
+    console.log("yeeh ajke movie dekhbo!!");
+else{
+    console.log("ghumabo ami.")
+}
+//6
+const temp=25;
+if(temp>30)
+{
+    console.log("AC calau!!");
+
+}
+else{
+    console.log("kOMBOL GAYE DIYE GHUMABO AMI zZZ..");
+}
