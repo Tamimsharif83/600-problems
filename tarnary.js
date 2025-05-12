396834a50fc3d39c476f85c0e6eef440c6521f59
Tamim Sharif <202314083@student.mist.ac.bd>
2025-05-13 02:52:30 +0600

    tarnary operatorice code added

---
+++ tarnary.js
@@ -0,0 +1,21 @@
+const shopAmount=3500;
+shopAmount>=3000?console.log("cashback 500 tk"):console.log("0 tk cashback");
+//1
+const age=21;
+age>=15?console.log("Teenager"):console.log("Child");
+//2
+const isLogedIn=false;
+isLogedIn===true?console.log("welcome back"):console.log("please login");
+//3
+const isFillUpFuelTank=true;
+isFillUpFuelTank===true?console.log("Ready for long drive"):console.log("Fill the tank");
+//4
+const isPassed=true;
+isPassed===true?console.log("party time"):console.log("Next semester e comeback dibo");
+//5
+const isSunny=true;
+isSunny===false?console.log("Stayhome"):console.log("Ghurte jabo");
+//6
+const  isExpensive=false;
+isExpensive===false?console.log("I will buy this item"):console.log("Kinbo nah");
+//7
