77a70343f204255d0e77bd8da7a2a61289aad76d
Tamim Sharif <202314083@student.mist.ac.bd>
2025-05-14 01:40:55 +0600

    array index practice code added

---
+++ array_index.js
@@ -0,0 +1,23 @@
+const friendName=['abul','kabul','kalam','shishir','alif'];
+console.log(friendName[3]);
+//1
+const favBook=['physics','3 a.m','bela furanar age','dowa kobuler golpo','ognibina','marhava javascript e maro thaba'];
+console.log(favBook[5]);
+//2
+const numArray=[1,2,3,4,5,6,7,8,9,10];
+numArray[7]=30;
+console.log(numArray);
+//3
+const favGame=['free fire','pubg','candy crush','tample run'];
+favGame[2]='sabway surfur';
+console.log(favGame);
+//4
+const familyMember=['abbu','ammu','mukut','borsha'];
+console.log(familyMember[3]);
+//5
+const dreamCountry=['Saudi Arabia','pakistan','Afganistan','Tibbat','katar','australia','turky'];
+console.log(dreamCountry[4]);
+//6
+const tableitem=['laptop','glass','water pot','book','khata'];
+console.log(tableitem[7]);
+//7
