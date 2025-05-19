77890348059995b1af8ce1015efeac34f7c5540c
Tamim Sharif <202314083@student.mist.ac.bd>
2025-05-19 14:27:09 +0600

    array includes functions  practice code added

---
+++ array_include.js
@@ -0,0 +1,29 @@
+const fruits=['apple','kola','am','lichu'];
+if(fruits.includes('am'))
+{
+    console.log('am ache');
+}
+else{
+    console.log('am nei, gache outh');
+}
+//1
+const Name=['babul','alif','choton'];
+console.log(Name.indexOf('babul'));
+//2
+const  friend=['rimon','rifat','rajib'];
+console.log(friend.indexOf('rifat'));
+//3
+const city=['dhaka','Chitagonog','shylet','rajshahi'];
+console.log(city.includes('RajShahi'));
+//4
+const array=['dighi','megh','bristy','borsha'];
+if(array.includes('bristy')){
+    console.log('I NEED UMBRELLA');
+}
+else{
+    console.log('no rain,no pain');
+}
+//5
+const games=['football','cricket','vollyball'];
+console.log(games.includes('badminton'));
+//6
