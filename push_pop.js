1c2e5e9157e2740b45c0741253b80b895b2626f3
Tamim Sharif <202314083@student.mist.ac.bd>
2025-05-19 14:06:55 +0600

    array push pop practice code added

---
+++ push_pop.js
@@ -0,0 +1,20 @@
+const numArray=[10,20,30,40,50];
+numArray.push(60);
+console.log(numArray);
+//1
+const frndNmae=['sajib','sagor','sakib','sohel'];
+frndNmae.push('sumon');
+console.log(frndNmae);
+//2
+const game=['Delta force', 'tample run','candy crush'];
+game.pop();
+console.log(game);
+//3
+const number=[24,36,48,60];
+number.unshift(12);
+console.log(number);
+//4
+const book=['lala','bala','kala'];
+book.shift();
+console.log(book);
+//5
