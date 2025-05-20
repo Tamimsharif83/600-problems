80d0b4a9aec81758a256ee2ee693bc3120a8d818
Tamim Sharif <202314083@student.mist.ac.bd>
2025-05-20 12:03:03 +0600

    forof loop  practice code added

---
+++ forOfLoop.js
@@ -0,0 +1,31 @@
+const favSub=['computer architecture','cyber security','electronics','math','physics'];
+for(const sb of favSub)
+{
+    console.log(sb);
+}
+//1
+const favFood=['biriyani','pizza','gorur mangso','vegetables'];
+for(const food of favFood)
+{
+    console.log(food);
+}
+//2
+const familyDob=[1972,1981,2004,2003,2011];
+for(const dob of familyDob)
+{
+    console.log(dob);
+
+}
+//3
+const favPlayer=['mahmudullah','Tamim','musfiq','shakib'];
+for(const player of favPlayer)
+{
+    console.log(player);
+}
+//4
+const xmDate=[16,19,22,25,29];
+for(const xm of xmDate)
+{
+    console.log(xm);
+}
+//5
