26359df017b17c3a036f3191478d7b8b88abfe82
Tamim Sharif <202314083@student.mist.ac.bd>
2025-05-22 01:33:07 +0600

    while loop  practice code added

---
+++ while.js
@@ -0,0 +1,32 @@
+let p=0;
+while(p<9)
+{
+    console.log('I will practice programmimg minimum 3 hour');
+    p++;
+}
+//1
+let num=1;
+while(num<=20){
+    console.log(num);
+    num++;
+}
+//2
+let number=50;
+while(number<=100){
+    console.log(number)
+    number++;
+}
+//3
+let n=1;
+while(n<=10){
+    console.log(n*3);
+    n++;
+}
+//4
+let div=111;
+while(div<=120)
+{
+    console.log(div/2);
+    div++;
+}
+//5
